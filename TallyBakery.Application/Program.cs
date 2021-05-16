5ffeefabf2ba60fd3a13398c3bedf58f07fc1b15
Aditya Wardhana <aditya.wardhana@advis.com.au>
2021-05-16 10:10:49 +1000

    taking input from user and parse it initialize product repository get maximum price break based on the input

--- TallyBakery.Application/Program.cs
+++ TallyBakery.Application/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TallyBakery.Application.Core;
 
 namespace TallyBakery
 {
@@ -6,10 +7,19 @@ namespace TallyBakery
     {
         static void Main(string[] args)
         {
-            ProductRepository repo = new ProductRepository();
-            repo.InitializeDatabase();
 
-            Console.ReadLine("Please enter your order (10 VS5)");
+            Console.WriteLine("Please enter your order (10 VS5)");
+            var input = Console.ReadLine();
+
+            var parts = input.Split(new char[] { ' ' });
+            var qty = int.Parse(parts[0]);
+            var code = parts[1];
+
+            ProductRepository repo = new ProductRepository();
+            repo.Initialization();
+
+            repo.GetMaxPriceBreakByProduct(code, qty);
+            //display max price breaks
         }
     }
 }
